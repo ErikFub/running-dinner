e6630f0544128a4657e4e1616c277ff6e81d3623
ErikFub <erik.fubel@gmail.com>
2021-12-21 10:53:43 +0000

    Add secrets manager

--- data_access/google_maps_api.py
+++ data_access/google_maps_api.py
@@ -1,12 +1,12 @@
 import requests
-from env.credentials_manager import CredentialsManager
+from utils.secrets_manager import SecretsManager
 
 
 class GoogleMapsAPI:
     def __init__(self):
         self.base_url = "https://maps.googleapis.com/maps/api/directions/"
         self.data_format = "json"
-        self.api_key = CredentialsManager.get_password('directions_api')
+        self.api_key = SecretsManager.get_password('directions_api')
 
     def get_distance(self, node, neighbor, mode: str = "transit"):
         response = requests.get(self.base_url + self.data_format, params={'origin': node,
