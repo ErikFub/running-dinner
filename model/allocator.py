46be34f625727d8c7a8d15a7ab17fa2fca8fc257
ErikFub <erik.fubel@gmail.com>
2021-12-21 12:40:11 +0000

    First model

---
+++ model/allocator.py
@@ -0,0 +1,8 @@
+class Allocator:
+    def __init__(self, matrix, n_rounds: int = 3, n_team_members: int = 2):
+        self.matrix = matrix
+        self.n_rounds = n_rounds
+        self.n_team_members = n_team_members
+
+    def get_best_allocations(self):
+        pass
