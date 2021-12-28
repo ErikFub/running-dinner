c432e7928497de67aa26d3596f36825f63e5b6b8
ErikFub <erik.fubel@gmail.com>
2021-12-28 15:01:10 +0100

    Change default transport mode to 'walking' and add option to retrieve polyline

--- data_access/google_maps_api.py
+++ data_access/google_maps_api.py
@@ -2,17 +2,25 @@ import requests
 from utils.secrets_manager import SecretsManager
 
 
-class GoogleMapsAPI:
-    def __init__(self):
+class DirectionsAPI:
+    def __init__(self, origin, destination, mode):
         self.base_url = "https://maps.googleapis.com/maps/api/directions/"
         self.data_format = "json"
         self.api_key = SecretsManager.get_password('directions_api')
+        self.response = self._get_response(origin, destination, mode)
+        self.distance = self._get_distance()
+        self.polyline = self._get_polyline()
 
-    def get_distance(self, node, neighbor, mode: str = "transit"):
-        response = requests.get(self.base_url + self.data_format, params={'origin': node + ' Lisbon',
-                                                                          'destination': neighbor + ' Lisbon',
+    def _get_response(self, origin, destination, mode):
+        response = requests.get(self.base_url + self.data_format, params={'origin': origin + ' Lisbon',
+                                                                          'destination': destination + ' Lisbon',
                                                                           'key': self.api_key,
                                                                           'mode': mode,
                                                                           'units': 'metric'})
-        response = response.json()
-        return response['routes'][0]['legs'][0]['duration']['value']
+        return response.json()
+
+    def _get_distance(self):
+        return self.response['routes'][0]['legs'][0]['duration']['value']
+
+    def _get_polyline(self):
+        return self.response['routes'][0]['overview_polyline']['points']
