5b517a85f5c20e62d6567ad3f1ece2ed4bc32518
ErikFub <erik.fubel@gmail.com>
2021-12-29 12:50:20 +0100

    Parallelize computations in pre-filtering

--- model/optimizer.py
+++ model/optimizer.py
@@ -5,6 +5,7 @@ from tqdm import tqdm
 from model.distance_matrix import DistanceMatrix
 from data_access.optimal_solution import OptimalSolutionAccess
 from data_access.precomputation import PrecomputationDataAccess
+from joblib import Parallel, delayed
 
 
 class Optimizer:
@@ -26,12 +27,12 @@ class Optimizer:
 
     def run(self, progress_bar: bool = False, prefilter: bool = False):
         if prefilter:
-            self.prefilter_nodes(load_precomputed=True)
+            self.prefilter_nodes(load_precomputed=False)
         iterator = tqdm(range(self.n_iter)) if progress_bar else range(self.n_iter)
         for i in iterator:
             allocator = Allocator(self.matrix, self.n_rounds, self.n_locations)
             allocations, nodes = allocator.get_feasible_solution()
-            costs = Evaluator(self.matrix, allocations, nodes).get_costs()
+            costs = Evaluator(self.matrix.distances, allocations, nodes).get_costs()
             if costs < self.lowest_costs:
                 self.best_allocation = allocations
                 self.best_allocation_teams = nodes
@@ -41,21 +42,23 @@ class Optimizer:
         if load_precomputed:
             filtered_teams = PrecomputationDataAccess().get_filtered_nodes()
         else:
-            reps = 10
-            base_optimization_iter_mult = 0.2
-            base_iter = int(base_optimization_iter_mult * self.n_iter)
-            best_team_constellations = []
-            for rep in range(reps):
+            def run_rep(matrix, n_rounds, n_locations, n_iter):
+                base_optimization_iter_mult = 1
+                base_iter = int(base_optimization_iter_mult * n_iter)
                 rep_costs = math.inf
                 rep_best_teams = None
-                for i in tqdm(range(base_iter)) if progress_bar else range(base_iter):
-                    allocator = Allocator(self.matrix, self.n_rounds, self.n_locations)
+                for i in range(base_iter):
+                    allocator = Allocator(matrix, n_rounds, n_locations)
                     allocations, nodes = allocator.get_feasible_solution()
-                    costs = Evaluator(self.matrix, allocations, nodes).get_costs()
+                    costs = Evaluator(matrix.distances, allocations, nodes).get_costs()
                     if costs < rep_costs:
                         rep_costs = costs
                         rep_best_teams = nodes
-                best_team_constellations.append(rep_best_teams)
+                return rep_best_teams
+
+            reps = 10
+            best_team_constellations = Parallel(n_jobs=5, verbose=20)\
+                (delayed(run_rep)(self.matrix, self.n_rounds, self.n_locations, self.n_iter) for i in range(reps))
             filtered_teams = list(set([t for constellation in best_team_constellations for t in constellation]))
             PrecomputationDataAccess().save_filtered_nodes(filtered_teams)
         print(f"Filtered out nodes: {self.n_participants - len(filtered_teams)}")
